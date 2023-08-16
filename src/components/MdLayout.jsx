4ba51f7c3abbec14dfbad81eb51193eba7c4a72e
Ricky_Pang <60353958+rickypang0219@users.noreply.github.com>
2023-08-17 01:04:09 +0800

    modify the equation size by config style.css

--- src/components/MdLayout.jsx
+++ src/components/MdLayout.jsx
@@ -8,7 +8,7 @@ import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dark, atomDark, oneLight, oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { Link } from "react-router-dom"
-
+import 'katex/dist/katex.min.css';
 
 const MdPost = ({ post }) => {
     return (
